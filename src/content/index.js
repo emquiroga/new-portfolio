22f39e81c3ab1d4fa66556ad9102fbe36ce1a609
emquiroga <emiliano.quiroga093@gmail.com>
2022-01-06 09:23:00 -0300

    updated projects

--- src/content/index.js
+++ src/content/index.js
@@ -1,27 +1,27 @@
 export const about = "Vivo en la ciudad de La Plata, Buenos Aires, Argentina. \n\nSoy un apasionado por los lenguajes, la tecnología y la comunicación. \n\nPoseo un nivel A2/B1 en idioma inglés y A1 en portugués. \n\nEncuentro en el desarrollo web un medio ideal para canalizar mis intereses, conocimientos y creatividad."
 export const work = "En la actualidad me encuentro trabajando como Web Developer freelancer. Antes de insertarme en el mundo IT, ejercí por casi diez años como profesional gastronómico y escritor. \n\nEn el primero, logré destacar en puestos de jerarquía que fortalecieron mis capacidades de administración y planificación grupal, a la vez que me ejercitaron en la toma de decisiones clave para el desempeño del negocio. \n\nOtras experiencias laborales relevantes en mi currículum incluyen Atención al Cliente, Data Entry y Corrector/Redactor."
 export const studies = "Desde el año 2011 hasta el año 2014, cursé la Licenciatura en Literaturas Modernas en la Universidad Nacional de Cuyo. En ese trayecto, participé como ayudante de cátedra en tres diferentes oportunidades, potenciando mis habilidades pedagógicas y de investigación. Durante los años 2019 y 2020 retomé dicha carrera en la Universidad Nacional de La Plata. \n\nDesde comienzos del 2021, decidí reorientar mis objetivos profesionales hacia la programación, con planes a futuro de cursar una tecnicatura en sistemas."
-export const techs = "Como Desarrollador Frontend, tengo conocimientos en JavaScript (ES6), React, Redux, React-Router, React-Testing (incipiente), HTML, CSS, Sass, Bootstrap, Materialize y otros frameworks de estilos. \n\nTambién cuento con conocimientos en bases de datos relacionales (MySQL), backend con PHP (Laravel) y desarrollo con arquitectura  MVC."
+export const techs = "Como Desarrollador Frontend, tengo conocimientos en JavaScript (ES6), React, Redux, React-Router, React-Testing (incipiente), HTML, CSS, Sass, Bootstrap, Materialize y otros frameworks de estilos. \n\nAdemás, poseo conocimientos en lenguaje PHP con Laravel, desarrollo con arquitectura MVC y manejo de bases de datos relacionales con MySQL."
 export const expectations = "Mi objetivo actual es poder insertarme en una empresa que me permita potenciar mis habilidades y canalizar mi entusiamo por el desarrollo web. \n\nDe igual manera, me gustaría poder seguir desempeñándome de manera freelance, en perspectiva de fortalecer mis conocimientos y ampliar mi red laboral. \n\nFinalmente, desearía a futuro poder expandir no solo mi stack de lenguajes y tecnologías, sino también incursionar en el diseño UI/UX, como también poder ofrecer mentorías y capacitaciones que canalicen al máximo mis aptitudes IT, creativas y pedagígicas."
 export const links = {
     "project1" : {
-        "live" : "https://loopstudios-fem-rho.vercel.app/",
-        "repo": "https://github.com/emquiroga/loopstudios-fem"
+        "live" : "http://arcane-wave-57857.herokuapp.com/",
+        "repo": "https://github.com/emquiroga/proyecto-fullstack"
     },
     "project2" : {
         "live" : "https://emquiroga.github.io/dimacofi-fullpage/",
         "repo": "https://github.com/emquiroga/dimacofi-fullpage"
     },
     "project3" : {
-        "live" : "https://buller-page.vercel.app/",
-        "repo": "https://github.com/emquiroga/buller-page"
+        "live" : "https://loopstudios-fem-rho.vercel.app/",
+        "repo": "https://github.com/emquiroga/loopstudios-fem"
     }   
 }
 export const projectDescriptions = {
-    "title1": "Loop Studios",
+    "title1": "Biblioteca Digital",
     "title2": "Dimacofi",
-    "title3": "Buller Page",
-    "pro1": "↳ SPA desarrollada con React.js y Sass como challenge pixel-perfect para el sitio Frontend Mentor",
-    "pro2": "↳ Maquetado freelance de las vistas para el cliente Dimacofi, desarrolladas con HTML, Sass y Bootstrap",
-    "pro3": "↳ Challenge pixel-perfect para estudio de diseño web. Desarrollada con React.js y Styled-Components"
+    "title3": "Loop Studios",
+    "pro1": "↳ App desarrollada con Laravel en el Backend y MySQL en DB. Las vistas del Frontend se desarrollaron con Blade, Bootstrap y Sass.",
+    "pro2": "↳ Maquetado freelance de las vistas para el cliente Dimacofi, desarrolladas con HTML, Sass y Bootstrap.",
+    "pro3": "↳ SPA desarrollada con React.js y Sass como challenge pixel-perfect para el sitio Frontend Mentor."
 }
